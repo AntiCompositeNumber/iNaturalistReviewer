f8cd009887bc93d7669af1bd42b97d59b0e9f4d4
AntiCompositeNumber <anticompositenumber@gmail.com>
2020-06-24 16:29:06 -0400

    Try with more memory requested, revert useless logging
    This reverts commit 770de6fd392595bf516d34a9fcd46d464245f209.

--- src/inrbot.py
+++ src/inrbot.py
@@ -23,7 +23,6 @@ import json
 import logging
 import logging.config
 import re
-import resource
 import string
 import time
 import urllib.parse
@@ -210,7 +209,6 @@ def find_photo_in_obs(
         else:
             for photo in photos:
                 logger.debug(f"Current photo: {photo}")
-                logger.debug(resource.getrusage(resource.RUSAGE_SELF))
                 res, ssim = compare_ssim(orig, photo)
                 if res:
                     return photo, f"ssim: {ssim:.4}"
@@ -543,7 +541,6 @@ def review_file(inpage: pywikibot.page.BasePage) -> Optional[bool]:
     except ValueError:
         return None
     logger.info(f"Checking {page.title(as_link=True)}")
-    logger.debug(resource.getrusage(resource.RUSAGE_SELF))
 
     utils.check_runpage(site, run_override)
     if not check_can_run(page):
