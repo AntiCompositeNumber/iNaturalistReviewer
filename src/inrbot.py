AntiCompositeNumber/iNaturalistReviewer

commit 3f24eef79876a73618ade327a43790ddbc844a13

Log errors on-wiki for untagged pages

src/inrbot.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-# Copyright 2020 AntiCompositeNumber
+# Copyright 2023 AntiCompositeNumber
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -44,7 +44,7 @@ from typing import Any, Iterator
 
 import acnutils
 
-__version__ = "2.3.2"
+__version__ = "2.3.3-beta.1"
 
 logger = acnutils.getInitLogger("inrbot", level="VERBOSE", filename="inrbot.log")
 
@@ -1008,6 +1008,34 @@ class CommonsPage:
             logger.info(summary)
             logger.info(message)
 
+    def log_untagged_error(self) -> None:
+        if simulate:
+            return
+        log_page = pywikibot.Page(site, config["untagged_log_page"])
+        if self.page.title() not in log_page.text:
+            message = string.Template(config["untagged_log_line"]).safe_substitute(
+                status=self.status,
+                reason=self.reason,
+                link=self.page.title(as_link=True, textlink=True),
+            )
+            summary = string.Template(config["untagged_log_summary"]).safe_substitute(
+                status=self.status,
+                reason=self.reason,
+                link=self.page.title(as_link=True, textlink=True),
+                version=__version__,
+            )
+            acnutils.check_runpage(site, override=run_override)
+            acnutils.retry(
+                acnutils.save_page,
+                3,
+                text=message,
+                page=log_page,
+                summary=summary,
+                bot=False,
+                minor=False,
+                mode="append",
+            )
+
     def review_file(
         self, throttle: Optional[acnutils.Throttle] = None
     ) -> Optional[bool]:
@@ -1068,6 +1096,7 @@ class CommonsPage:
             # Not previously tagged, don't need to throw an error message on it.
             logger.info("Skipping...")
             skip.add(self.page.title())
+            self.log_untagged_error()
             # TODO: report out failures/maintain skip list
 
             return False

src/inrcli.py
@@ -17,6 +17,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import acnutils
 import click
 import pywikibot  # type: ignore
 import pywikibot.bot  # type: ignore
@@ -75,19 +76,6 @@ inrbot.compare_methods.insert(0, ("manual", manual_compare))
 class ManualCommonsPage(inrbot.CommonsPage):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-    def check_can_run(self) -> bool:
-        """Determinies if the bot should run on this page and returns a bool."""
-        page = self.page
-        if (
-            (page.title() in inrbot.skip)
-            or (not page.has_permission("edit"))
-            or (not page.botMayEdit())
-            or (not re.search("{{[iI][nN]aturalist[rR]eview", page.text))
-        ):
-            return False
-        else:
-            return True
 
     def get_old_archive_(self):
         return super().get_old_archive()
@@ -173,6 +161,42 @@ class ManualCommonsPage(inrbot.CommonsPage):
         ids[self.page] = ina_id
         return ina_id
 
+    def log_untagged_error(self) -> None:
+        # Errors while running in CLI do not need to be logged on-wiki
+        return
+
+    def remove_untagged_log(self) -> None:
+        log_page = pywikibot.Page(site, inrbot.config["untagged_log_page"])
+        new_text, changes = re.subn(
+            r"^.*?{0}.*\n?".format(self.page.title()),
+            "",
+            log_page.text,
+            flags=re.MULTILINE,
+        )
+        summary = (
+            f"Removing {self.page.title(as_link=True, textlink=True)} "
+            f"(inrcli {inrbot.__version__})"
+        )
+        if changes == 0:
+            return
+        if inrbot.simulate:
+            logger.debug(summary)
+            logger.debug(new_text)
+        else:
+            acnutils.retry(
+                acnutils.save_page,
+                3,
+                text=new_text,
+                page=log_page,
+                summary=summary,
+                bot=False,
+                minor=False,
+            )
+
+    def review_file(self, *args, **kwargs) -> None:
+        if super().review_file(*args, **kwargs):
+            self.remove_untagged_log()
+
 
 inrbot.id_hooks.append(ManualCommonsPage.id_hook)
 inrbot.lock_hooks.append(ManualCommonsPage.archive_status_hook)
@@ -196,6 +220,15 @@ def main(target, url="", simulate=False, reverse=False):
                 continue
             ManualCommonsPage(pywikibot.FilePage(page)).review_file()
             click.confirm("Continue", abort=True, default=True)
+    elif target == "errors":
+        log_page = pywikibot.Page(site, inrbot.config["untagged_log_page"])
+        dtt = pywikibot.Page(site, "Template:Deletion template tag")
+        for page in log_page.linkedPages(namespaces=6, follow_redirects=True):
+            if not page.exists() or dtt in set(page.itertemplates()):
+                ManualCommonsPage(pywikibot.FilePage(page)).remove_untagged_log()
+                continue
+            ManualCommonsPage(pywikibot.FilePage(page)).review_file()
+            click.confirm("Continue", abort=True, default=True)
     elif target == "ask":
         while True:
             new_target = click.prompt("Target", default="")
@@ -208,7 +241,7 @@ def main(target, url="", simulate=False, reverse=False):
             # TODO: Add validation
             ids[page] = inrbot.parse_ina_url(url)
 
-        ManualCommonsPage(pywikibot.FilePage(page)).review_file()
+        ManualCommonsPage(page).review_file()
 
 
 if __name__ == "__main__":

tests/test_inrbot.py
@@ -1133,6 +1133,7 @@ def test_review_file_error_untagged(runpage):
     mock_page = mock.Mock(spec=pywikibot.FilePage, text="{{iNaturalistreview}}")
     cpage = inrbot.CommonsPage(mock_page)
     mock_review = mock.Mock()
+    log_untagged_error = mock.Mock()
     with mock.patch.multiple(
         cpage,
         check_can_run=mock.Mock(return_value=True),
@@ -1140,11 +1141,13 @@ def test_review_file_error_untagged(runpage):
         update_review=mock_review,
         find_ina_id=mock.Mock(side_effect=exc),
         check_has_template=mock.Mock(return_value=False),
+        log_untagged_error=log_untagged_error,
     ):
         cpage.review_file()
     assert cpage.status == "error"
     assert cpage.reason == reason
     mock_review.assert_not_called()
+    log_untagged_error.assert_called_once()
     runpage.assert_called()
 
 

commit 9afcc3dbad37f45a1c8161a87a9f58e2f0241394

Re-override check_can_run in ManualCommonsPage
inrcli should run on previously-reviewed files. This was broken in
3f24eef.

Also fix CommonsPage.check_has_template to include reviewed files.

src/inrbot.py
@@ -44,7 +44,7 @@ from typing import Any, Iterator
 
 import acnutils
 
-__version__ = "2.4.2"
+__version__ = "2.4.3"
 
 logger = acnutils.getInitLogger("inrbot", level="VERBOSE", filename="inrbot.log")
 
@@ -475,7 +475,7 @@ class CommonsPage:
             return True
 
     def check_has_template(self) -> bool:
-        return bool(re.search(r"{{[iI][nN]aturalist[rR]eview}}", self.page.text))
+        return bool(re.search(r"{{[iI][nN]aturalist[rR]eview", self.page.text))
 
     def check_stop_cats(self) -> None:
         stop_cats = {

src/inrcli.py
@@ -81,6 +81,18 @@ class ManualCommonsPage(inrbot.CommonsPage):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    def check_can_run(self) -> bool:
+        """Determinies if the bot should run on this page and returns a bool."""
+        page = self.page
+        if (
+            (page.title() in inrbot.skip)
+            or (not page.has_permission("edit"))
+            or (not page.botMayEdit())
+        ):
+            return False
+        else:
+            return True
+
     def get_old_archive_(self):
         return super().get_old_archive()
 
@@ -209,7 +221,8 @@ def main(target, url="", simulate=False, reverse=False):
             if dtt in set(page.itertemplates()):
                 continue
             try:
-                ManualCommonsPage(pywikibot.FilePage(page)).review_file()
+                mcp = ManualCommonsPage(pywikibot.FilePage(page))
+                mcp.review_file()
             except SkipFile:
                 continue
     elif target == "errors":